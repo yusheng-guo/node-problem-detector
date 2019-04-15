dbc4cd28446668a8225655fa0bd6dc374330264e
ringtail <zhongwei.lzw@alibaba-inc.com>
2019-04-15 21:21:07 +0800

    fix warn and info level of custom plugin

--- config/plugin/check_fd.sh
+++ config/plugin/check_fd.sh
@@ -6,19 +6,14 @@ OK=0
 NONOK=1
 UNKNOWN=2
 
-which lsof > /dev/null
-if [ $? -ne 0 ]; then
-    echo "lsof is not support"
-    exit $UNKNOWN
-fi
+cd /host/proc
 
-count=$(lsof | wc -l)
-max=$(cat /proc/sys/fs/file-max)
+count=$(find -maxdepth 1 -type d -name '[0-9]*' | xargs -I {} ls {}/fd | wc -l)
+max=$(cat /host/proc/sys/fs/file-max)
 
-echo "current fd is $count and max is $max"
-if [[ $count -gt $((max*8/10)) ]]; then
-   echo "more than 80 percent of fd has been used($count:$max)"
+if [[ $count -gt $((max*80/100)) ]]; then
+   echo "current fd usage is $count and max is $max"
    exit $NONOK
 fi
-echo "fd is ok"
+echo "node has no fd pressure"
 exit $OK
